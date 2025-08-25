7dd915a54fe1b546e90d28fb75714f1b71aca5d8
xseniaKri <kseniaenta@mail.ru>
2025-08-25 16:57:42 +0300

    feat: добавила модели юзера, цитаты, источника и голоса

--- quotesapp/admin.py
+++ quotesapp/admin.py
@@ -1,3 +1,26 @@
 from django.contrib import admin
+from .models import User, Quote, Source, Vote
 
-# Register your models here.
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('nickname', 'email', 'password')
+
+@admin.register(Quote)
+class QuoteAdmin(admin.ModelAdmin):
+    list_display = ('source', 'text', 'weight', 'views', 'likes_count', 'dislikes_count')
+
+    def likes_count(self, obj):
+        return obj.likes_count()
+    likes_count.short_description = "Лайки"
+
+    def dislikes_count(self, obj):
+        return obj.dislikes_count()
+    dislikes_count.short_description = "Дизлайки"
+
+@admin.register(Source)
+class SourceAdmin(admin.ModelAdmin):
+    list_display = ('category', 'name')
+
+@admin.register(Vote)
+class VoteAdmin(admin.ModelAdmin):
+    list_display = ('quote', 'user', 'value')
--- quotesapp/models.py
+++ quotesapp/models.py
@@ -1,3 +1,39 @@
 from django.db import models
 
-# Create your models here.
+
+class User(models.Model):
+    nickname = models.CharField(max_length=25, help_text="Введите логин (Xsenia)")
+    password = models.BinaryField()
+    email = models.EmailField(help_text="Введите почту (example@mail.ru)")
+
+class Source(models.Model):
+    category = models.CharField(max_length=255, help_text="Введите категорию (книга, фильм и т. д.)")
+    name = models.CharField(max_length=255, help_text="Введите название источника (Властелин колец)")
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(fields=["category", "name"], name="unique_category_name"),
+        ]
+
+class Quote(models.Model):
+    source = models.ForeignKey(Source, on_delete=models.CASCADE, related_name="quotes")
+    text = models.CharField(max_length=255, help_text="Введите цитату (Быть или не быть - вот, в чем вопрос)")
+    weight = models.PositiveIntegerField(default=1, help_text="Введите вес цитаты (от 1 до 10)")
+    views = models.PositiveIntegerField(default=0)
+    creator = models.ForeignKey(User, on_delete=models.CASCADE, related_name="quotes")
+
+    def likes_count(self):
+        return self.votes.filter(value=1).count() # type: ignore
+
+    def dislikes_count(self):
+        return self.votes.filter(value=-1).count() # type: ignore
+
+class Vote(models.Model):
+    quote = models.ForeignKey(Quote, on_delete=models.CASCADE, related_name="votes")
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="votes")
+    value = models.SmallIntegerField(choices=[(-1, "Dislike"), (1, "Like")])
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(fields=["quote", "user"], name="unique_quote_user"),
+        ]
